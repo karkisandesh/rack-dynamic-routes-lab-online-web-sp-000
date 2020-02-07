a25de43aa544d55d920ad5987166a6b54cc5928f
Sandesh Karki <karkisandesh@hotmail.com>
2020-02-07 06:40:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    @@items << Item.new()
-    
-    item_name = @@items.each {|i| i.name}
-    
-    if req.path =="/items/item_name"
+  
+    if req.path =="/items/"
       resp.write "#{item_name.price}"
     else
       resp.write "Item not found"

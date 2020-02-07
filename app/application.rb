e8a43e6add0a04852e3d18e28143a8ba17fd4bcd
Sandesh Karki <karkisandesh@hotmail.com>
2020-02-07 06:48:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,13 +11,8 @@ class Application
       item_name = @@items.find{|i| i.name == item}
 
     if req.path =="/items/item_name"
-
-      if item_name.nil?
-        resp.write "Item not found"
-        resp.status = 400
-      else
+        !item_name.nil?
         resp.write "#{item_name.price}"
-      end
     else
       resp.write "Item not found"
       resp.status = 404

c2ccd17ef258bdd0da5b342604bdd764a9110c7d
Sandesh Karki <karkisandesh@hotmail.com>
2020-02-07 06:47:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "#{item_name.price}"
       end
     else
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 404
     end
 
